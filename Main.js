10107c78a568eccd9c343d578d7bc39d41947849
treeshater <warinmario8@gmail.com>
2026-03-15 13:43:51 +0100

    Update Main.js

--- Main.js
+++ Main.js
@@ -2,24 +2,25 @@
 let trees = 0
 let treeGain = 1
 let treeAutomator = false
-let treeAutomatorEffeciency = 200
+let treeAutomatorEffeciency = 100
 let treeAutomatorPrice = 50
 let totaltrees = 0
 
 function Treecut() {
-    if (treeAutomator === false) {
     trees = trees + treeGain
     totaltrees = totaltrees + treeGain
     document.getElementById('IDd').innerHTML = trees + " logs"
-    }
-    else {
-        setInterval (() => {
-            trees = trees + treeGain
-            totaltrees = totaltrees + treeGain
-        }, treeAutomatorEffeciency
-    )
-    }
 
+}
+
+function check() {
+    if (treeAutomator === true) {
+         setInterval (() => {
+             trees = (trees * 10 + treeGain) / 10
+              totaltrees = (totaltrees * 10 + treeGain * 10) / 10
+                document.getElementById('IDd').innerHTML = trees + " logs"
+          }, treeAutomatorEffeciency )
+    }
 }
 
 function BuyAutomator() {
@@ -27,15 +28,18 @@ function BuyAutomator() {
         { if (treeAutomator === false)
              {
                 treeAutomator = true
+                trees = trees - treeAutomatorPrice
+                document.getElementById('IDd').innerHTML = trees + " logs"
                 treeAutomatorPrice = treeAutomatorPrice * 2.5
+                check()
               }
               else { treeAutomatorEffeciency = treeAutomatorEffeciency / 1.1
-                treeAutomatorPrice = treeAutomatorPrice * 2.5
+                   trees = trees - treeAutomatorPrice
+                   document.getElementById('IDd').innerHTML = trees + " logs"
+                   treeAutomatorPrice = treeAutomatorPrice * 2.5
               }
                
     }
     
     
 }
-
-
