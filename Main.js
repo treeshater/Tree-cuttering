fdd09776f8dd6d52b210c4700626474e6f6de86d
treeshater <warinmario8@gmail.com>
2026-03-16 23:34:16 +0100

    Added omeganum.js

--- Main.js
+++ Main.js
@@ -1,24 +1,37 @@
 
-let trees = 0
-let treeGain = 1
+
+
+let trees =  OmegaNum("0")
+let treeGain =  OmegaNum("0.10")
 let treeAutomator = false
 let treeAutomatorEffeciency = 100
-let treeAutomatorPrice = 50
-let totaltrees = 0
+let treeCutters =  OmegaNum("0")
+let treeAutomatorPrice =  OmegaNum("50")
+let totaltrees =  OmegaNum("1")
+
+function treegainCalculate(x) {
+    x = OmegaNum(0.10)
+    if (treeCutters >= 1) {
+        x.add((treeCutters).div(10))
+    } 
+}
 
 function Treecut() {
-    trees = trees + treeGain
-    totaltrees = totaltrees + treeGain
-    document.getElementById('IDd').innerHTML = trees + " logs"
+    treegainCalculate(treeGain)
+    trees = trees.add(treeGain)
+    totaltrees = totaltrees.add(treeGain)
+       document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
 
 }
 
 function check() {
     if (treeAutomator === true) {
          setInterval (() => {
-             trees = (trees * 10 + treeGain) / 10
-              totaltrees = (totaltrees * 10 + treeGain * 10) / 10
-                document.getElementById('IDd').innerHTML = trees + " logs"
+            treegainCalculate(treeGain)
+             trees = trees.add(treeGain)
+             Math.round(trees * 10 ^ 2) / 10 ^ 2
+             totaltrees = totaltrees.add(treeGain)
+                document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
           }, treeAutomatorEffeciency )
     }
 }
@@ -28,18 +41,28 @@ function BuyAutomator() {
         { if (treeAutomator === false)
              {
                 treeAutomator = true
-                trees = trees - treeAutomatorPrice
-                document.getElementById('IDd').innerHTML = trees + " logs"
-                treeAutomatorPrice = treeAutomatorPrice * 2.5
+                trees = trees.sub(treeAutomatorPrice)
+                document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
+                treeAutomatorPrice = treeAutomatorPrice.mul(2.5)
                 check()
               }
               else { treeAutomatorEffeciency = treeAutomatorEffeciency / 1.1
-                   trees = trees - treeAutomatorPrice
-                   document.getElementById('IDd').innerHTML = trees + " logs"
-                   treeAutomatorPrice = treeAutomatorPrice * 2.5
+                   trees = trees.sub(treeAutomatorPrice)
+                   document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
+                   treeAutomatorPrice = treeAutomatorPrice.mul(2.5)
               }
                
     }
     
     
+}
+
+let treeCutterPrice = OmegaNum(5)
+function buyCutter() {
+    if (trees >= treeCutterPrice) {
+        trees.sub(treeCutterPrice)
+        treeCutters.add(1)
+        treeCutterPrice.mul(1.35)
+        document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
+    }
 }
