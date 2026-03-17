ed9ff14f01edc18fa582370eb5bdd29420cbbfe9
treeshater <warinmario8@gmail.com>
2026-03-17 17:04:59 +0100

    Updated functions and the style

--- Main.js
+++ Main.js
@@ -1,19 +1,19 @@
-
-
-
 let trees =  OmegaNum("0")
-let treeGain =  OmegaNum("0.10")
+let treeGain =  OmegaNum("0")
 let treeAutomator = false
 let treeAutomatorEffeciency = 100
 let treeCutters =  OmegaNum("0")
 let treeAutomatorPrice =  OmegaNum("50")
 let totaltrees =  OmegaNum("1")
+let treeCutterPrice = OmegaNum("5")
 
-function treegainCalculate(x) {
-    x = OmegaNum(0.10)
+function treegainCalculate() {
+    x = OmegaNum("0.10")
     if (treeCutters >= 1) {
-        x.add((treeCutters).div(10))
+        x = x.add(treeCutters.div(10))
     } 
+    x = x.mul(burnerEffect)
+    treeGain = x
 }
 
 function Treecut() {
@@ -29,10 +29,18 @@ function check() {
          setInterval (() => {
             treegainCalculate(treeGain)
              trees = trees.add(treeGain)
-             Math.round(trees * 10 ^ 2) / 10 ^ 2
              totaltrees = totaltrees.add(treeGain)
                 document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
           }, treeAutomatorEffeciency )
+    }
+}
+
+function buyCutter() {
+    if (trees >= treeCutterPrice) {
+        trees = trees.sub(treeCutterPrice)
+        treeCutters = treeCutters.add(1)
+        treeCutterPrice = treeCutterPrice.mul(1.35)
+        document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
     }
 }
 
@@ -57,12 +65,19 @@ function BuyAutomator() {
     
 }
 
-let treeCutterPrice = OmegaNum(5)
-function buyCutter() {
-    if (trees >= treeCutterPrice) {
-        trees.sub(treeCutterPrice)
-        treeCutters.add(1)
-        treeCutterPrice.mul(1.35)
+let BurnerCost = OmegaNum("25")
+let Burners = OmegaNum("0")
+let burnerEffect = OmegaNum("1")
+
+function BuyBurner() {
+    if (trees >= BurnerCost) {
+        Burners = Burners.add(1)
+        trees = trees.sub(BurnerCost)
+        BurnerCost = BurnerCost.mul(1.75)
+        burnerEffect = burnerEffect.mul(1.25)
         document.getElementById('IDd').innerHTML = trees.toFixed(2) + " logs"
+
     }
 }
+
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # treeshater.github.io
+A web incremental about cutting a bunch of trees and ruining the planet.
